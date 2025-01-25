569c34bcd85e934a1d845b55b4423b816661f6e5
schakibb <schakibhad@gmail.com>
2025-01-25 12:18:13 +0100

    feat: implement ProductController with CRUD operations

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -15,4 +15,36 @@ public class ProductController {
     private ProductService productService;
 
 
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping(path ="{id}" )
+    public Product getProductById(@PathVariable  int id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping(path = "/add-product")
+    public Product addProduct(@RequestBody Product product) {
+        product.setUpdatedAt(LocalDateTime.now());
+        product.setCreatedAt(LocalDateTime.now());
+        return productService.addProduct(product);
+    }
+
+    @PutMapping(path="/{id}")
+    public Product updateProduct(@PathVariable Integer id, @RequestBody Product updatedProduct) {
+        return productService.updateProduct(id,updatedProduct);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+    }
+
+    @GetMapping("/search")
+    public List<Product> searchProducts(@RequestParam String product_name) {
+        return productService.searchByName(product_name);
+    }
 }
